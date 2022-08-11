799123e1404096cdd9498e7c1429fe334805d337
Nick Martin <nicholas.martin@tharsus.co.uk>
2022-08-11 08:00:50 +0100

    update version correctly

--- mod.ts
+++ mod.ts
@@ -5,5 +5,5 @@ export { work } from "./work.ts";
 export { publications } from "./publications.ts";
 export { bio, importantInfo, examples } from "./bot.ts";
 
-const version = "1.1.2";
+const version = "1.1.5";
 export default version;
